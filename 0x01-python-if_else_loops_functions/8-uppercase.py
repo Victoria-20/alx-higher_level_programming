27e9b0ac86c05476a4b28bfe4cab07ad8b47ea22
Victoria-20 <victoriaocon111@gmail.com>
2022-06-02 10:14:14 +0300

    8-uppercse correction1

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+def upper(c):
+    asci = ord(c)
+    if asci >= 97 and asci <= 127:
+        return (chr(65 + (asci - 97)))
+    else:
+        return(c)
+
+
 def uppercase(str):
-    print(str.upper())
+    for loop in str:
+        print("{}".format(upper(loop)), end="")
+    print("")

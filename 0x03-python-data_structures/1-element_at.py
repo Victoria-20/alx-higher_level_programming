62b9e6137bad7c99adae1d7c83910067ce159ea1
Victoria-20 <victoriaocon111@gmail.com>
2022-06-06 10:07:57 +0300

    1. Access element correction1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,11 +2,7 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx > len(my_list):
-        return None
+    if idx < 0 or idx >= len(my_list):
+        return (None)
     else:
-        for item in my_list:
-            if item == my_list[idx]:
-                return item
+        return my_list[idx]

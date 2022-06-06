0ed4109146b13d6bdb9d008e49a3dccf4fc8867d
Victoria-20 <victoriaocon111@gmail.com>
2022-06-06 10:00:06 +0300

    1. Access element in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > len(my_list):
+        return None
+    else:
+        for item in my_list:
+            if item == my_list[idx]:
+                return item

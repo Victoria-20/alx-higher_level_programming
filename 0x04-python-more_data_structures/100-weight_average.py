0aef8a60d72c159a6fe150ab962ff9fe8b9aa772
Victoria-20 <victoriaocon111@gmail.com>
2022-06-10 11:10:28 +0300

    13. Weighted average correction1

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,7 +5,10 @@ def weight_average(my_list=[]):
     sum = 0
     weighted_sum = 0
 
-    for i in my_list:
-        sum = sum + i[0] * i[1]
-        weighted_sum = weighted_sum + i[1]
-    return sum / weighted_sum
+    if my_list:
+        for i in my_list:
+            sum = sum + i[0] * i[1]
+            weighted_sum = weighted_sum + i[1]
+        return sum / weighted_sum
+    else:
+        return 0

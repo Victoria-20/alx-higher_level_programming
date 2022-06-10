88cbff5c95bb962ee3e90e049367e2c313c5b939
Victoria-20 <victoriaocon111@gmail.com>
2022-06-10 10:04:12 +0300

    12. Roman to interger

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 
 
+def roman_to_int(roman_string):
+    base_rom_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                     'C': 100, 'D': 500, 'M': 100}
+    int_string = ''
+
+    for i in roman_string:
+        int_string += str(base_rom_dict[i])
+    return int(int_string)

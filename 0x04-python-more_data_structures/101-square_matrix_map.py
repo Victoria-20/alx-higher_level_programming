60ab2df341cc268527983258c0a2dc7742b37c4a
Victoria-20 <victoriaocon111@gmail.com>
2022-06-10 11:25:33 +0300

    14. Squared by using map - advanced

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-
-
-def roman_to_int(roman_string):
-    roman_string = roman_string.upper()
-    rom_dict = {'M': 1000, 'D': 500, 'C': 100,
-                'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    int_sum = 0
-    for i in range(len(roman_string)):
-        if i > 0 and rom_dict[roman_string[i]] > rom_dict[roman_string[i - 1]]:
-            int_sum += rom_dict[roman_string[i]] - 2 * rom_dict[roman_string[i - 1]]
-        else:
-            int_sum += rom_dict[roman_string[i]]
-    return int_sum
+def square_matrix_map(matrix=[]):
+    return list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix))

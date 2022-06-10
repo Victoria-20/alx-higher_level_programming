0d84ea2a33bfe51cf3408a18feb47a40c2ff61dc
Victoria-20 <victoriaocon111@gmail.com>
2022-06-10 11:06:47 +0300

    13. Weighted average - advanced

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    sum = 0
+    weighted_sum = 0
+
+    for i in my_list:
+        sum = sum + i[0] * i[1]
+        weighted_sum = weighted_sum + i[1]
+    return sum / weighted_sum

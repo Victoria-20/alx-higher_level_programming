4b78b08f11b8d4f4c449767348b6dbb3880febe8
Victoria-20 <victoriaocon111@gmail.com>
2022-06-21 11:22:26 +0300

    0.Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    num = 0
+    try:
+        for element in my_list[0:x]:
+            print(element, end="")
+            num = num + 1
+
+    except IndexError:
+        print("")
+    print()
+    return num

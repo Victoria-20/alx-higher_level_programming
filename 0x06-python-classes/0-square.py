5ec4004e9c95699fc3c1fcf34e67bf960150a6c9
Victoria-20 <victoriaocon111@gmail.com>
2022-06-22 16:25:03 +0300

    0. My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
+"""Module Docstring.
+This module demostrates how to create an empty class named Square
+that defines a square.
+
+"""
+
+
 class Square:
-    """Defines an empty square"""
+    """Defines an empty square
+    Args:
+        No parameters
+    Attributes:
+        No attributes
+
+    """
     pass

1665eb9cbe5aeb05b6000e462cc5d56b49eaa083
Victoria-20 <victoriaocon111@gmail.com>
2022-06-22 17:01:01 +0300

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 
+"""Module Docstring.
+This module demostrates how to create a class named Square
+that defines a square.
+
+"""
+
+
 class Square:
-   """Defines an empty square"""
-    pass
+    """Defines a square
+    Args:
+        No parameters
+    Attributes:
+        No attributes
+
+    """
+    def __init__(self, size):
+        """Initializes a new square
+        Args:
+            size (int): size of the square"""
+        self.__size = size

c7a6031eb06f6f8372e65abccf75a6e1d6cc6a8f
Victoria-20 <victoriaocon111@gmail.com>
2022-06-23 11:11:05 +0300

    8.Print square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -78,12 +78,24 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """prints in stdout the square with the character #
-        Args:
-        Returns:
-        Examples:
-        """
+        """Print the square with the # character."""
         if self.__size == 0:
-            print()
-        else:
-            print(f"{self.__size * '#'}")
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
+
+    def __str__(self):
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")

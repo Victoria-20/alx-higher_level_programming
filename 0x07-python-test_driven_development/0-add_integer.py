eb86e41dd25d8a8b1a693cf6d519858667e86826
Victoria-20 <victoriaocon111@gmail.com>
2022-06-27 12:03:39 +0300

    0.Integer addition correction1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+""" Calculation module """
 
 def add_integer(a, b=98):
-    if type(a, b) not in [int, float]:
-        raise TypeError("a must be an integer" or "b must be an integer")
-    return a + b
+    """ function to return sum of two integers """
+
+    test_a = type(a) in [int, float]
+    test_b = type(b) in [int, float]
+
+    if test_a and test_b:
+        return a + b
+    elif test_a:
+        raise TypeError("a must be an integer")
+    elif test_b:
+        raise TypeError("b must be an integer")

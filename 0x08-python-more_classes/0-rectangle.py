a4cbf9106043c250f3cc53c128248f0d754e9010
Victoria-20 <victoriaocon111@gmail.com>
2022-06-28 08:48:24 +0300

    0. Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Module Docstring.
+This module demostrates how to create an empty class named Rectangle
+that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """Defines an empty Rectangle
+    Args:
+        No parameters
+    Attributes:
+        No attributes
+    """
+    pass

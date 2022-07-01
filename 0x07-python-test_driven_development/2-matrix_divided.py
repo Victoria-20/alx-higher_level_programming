94e041cbe3959b7e6ab9df52435d9133d60bf412
Victoria-20 <victoriaocon111@gmail.com>
2022-07-01 09:50:43 +0300

    2.Say my name

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,29 @@
 #!/usr/bin/python3
 
 
+def matrix_divided(matrix, div):
+    """Divides all the values of the matrix"""
+    matrix_error = "matrix must be a matrix (list of lists) of integers/floats"
+    if matrix is None or len(matrix) == 0 or len(matrix[0]) == 0:
+        raise TypeError(matrix_error)
+
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    results_matrix = []
+    for list in matrix:
+        if type(list) != list:
+            raise TypeError(matrix_error)
+
+        if len(list) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        c_resList = []
+        for item in list:
+            if type(item) != int and type(item) != float:
+                raise TypeError(matrix_error)
+            c_resList.append(round(item/div, 2))
+        results_matrix.append(c_resList)
+    return results_matrix
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+prints strings
+"""
 
 
+def say_my_name(first_name, last_name=""):
+    """ Prints first_name and last_name """
+    if not type(first_name) is str:
+        raise TypeError("first_name must be a string")
+    elif not type(last_name) is str:
+        raise TypeError("last_name must be a string")
+    else:
+        print(f"My name is {first_name} {last_name}")

347235ef59b65cc0e53bb03ccf0a89f47c02aac6
Victoria-20 <victoriaocon111@gmail.com>
2022-07-01 10:13:10 +0300

    3.Print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""prints square"""
 
 
+def print_square(size):
+    if not type(size) is int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()

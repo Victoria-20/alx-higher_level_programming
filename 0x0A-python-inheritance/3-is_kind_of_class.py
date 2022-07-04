ed138d5df1343bbf6d9a28a97fea9fabb4ab2b6f
Victoria-20 <victoriaocon111@gmail.com>
2022-07-04 13:25:24 +0300

    Add 3.Same class doc

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" Module to check if same class or inherited """
+
 
 def is_kind_of_class(obj, a_class):
+    """Return True if object is an instance otherwise False """
     if isinstance(obj, a_class):
         return True
     else:

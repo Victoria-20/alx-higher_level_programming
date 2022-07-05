ec3c6b929ff60e5b2718426860dcb9e7ef23f163
Victoria-20 <victoriaocon111@gmail.com>
2022-07-05 09:42:02 +0300

    Add advanced tasks

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+""" Module to set attribute """
 
-class MyInt(int):
-    pass
+
+def add_attribute(obj, obj_name, value):
+    """add attribute to object
+    args:
+        obj: class object
+        obj_name: object name
+        value: value of attribute
+    return:
+    """
+    if hasattr(obj, "__dict__") is False:
+        raise TypeError("can't add new attribute")
+    setattr(obj, obj_name, value)

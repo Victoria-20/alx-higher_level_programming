d8d70133f116447cb7927baf25a1c020db4e7299
Victoria-20 <victoriaocon111@gmail.com>
2022-07-05 09:32:52 +0300

    11.Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" Module to calculate a Square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Define a class Square
+       Args:
+    """
+
+    def __init__(self, size):
+        """Validates size """
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"

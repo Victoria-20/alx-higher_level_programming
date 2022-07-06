1d1166730508dc198c4d06e719531eed00f9308e
Victoria-20 <victoriaocon111@gmail.com>
2022-07-06 09:45:09 +0300

    0.Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" Module to read a file"""
+
+
+def read_file(filename=""):
+    """ Reads a file and print to stdout """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end = "")

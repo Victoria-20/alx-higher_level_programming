6da46d4f76f4324a8a9074ce82c1e2a13e44ceb1
Victoria-20 <victoriaocon111@gmail.com>
2022-07-06 11:34:49 +0300

    students to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" Module for Class Student """
+
+
+class Student:
+    """
+        Defines a student class:
+        Args:
+            first_name (str): student firstname
+            last_name (str): student surname
+            age (int): student age
+        Methods:
+            __init__ - initializes the Student instance
+            to_json - retrieves dictionary desc of Student instance
+    """
+    def __init__(self, first_name, last_name, age):
+        """ Initialises Student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns dictionary representation of Student """
+        return self.__dict__

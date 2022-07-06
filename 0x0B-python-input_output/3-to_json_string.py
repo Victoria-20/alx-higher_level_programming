d6a5b3427451164fa0461af7fd30235e2d502a8f
Victoria-20 <victoriaocon111@gmail.com>
2022-07-06 10:39:09 +0300

    To JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Module to return JSON representation """
+import json
+
+
+def to_json_string(my_obj):
+    """ Returns the JSON reprensenation of an object"""
+    return json.dumps(my_obj)

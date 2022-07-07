227502992cce1bb4265ce946f4fbced2d88ad2ee
Victoria-20 <victoriaocon111@gmail.com>
2022-07-07 08:50:31 +0300

    Log parsing

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,7 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text when searched string is found """
     with open(filename, 'r') as f:
         lines = f.readlines()
     with open(filename, 'w') as f:

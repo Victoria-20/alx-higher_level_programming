3cfe51aacfe6f6d369bc550d31e8318bf322c5db
Victoria-20 <victoriaocon111@gmail.com>
2022-08-10 16:35:05 +0300

    More MySQL querries

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,1 +1,3 @@
--- Lists all databases of your MySQL server
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+SELECT score, name FROM second_table ORDER BY score DESC
+

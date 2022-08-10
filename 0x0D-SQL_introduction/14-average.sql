24c79e3df53f228dc1bf30bc477e20da9efb434a
Victoria-20 <victoriaocon111@gmail.com>
2022-08-10 17:21:10 +0300

    More MySQL querries 2

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,1 +1,2 @@
--- Lists all databases of your MySQL server
+-- computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
+SELECT AVG(score) AS average FROM second_table
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- Prints the full description of the table first_table from the databases hbtn_0c_0 in your MySQL server
-sp_help first_table;
+SHOW CREATE TABLE first_table;

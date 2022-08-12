453c9971f81c01980edb6a99e9b077e4fe11e4a2
Victoria-20 <victoriaocon111@gmail.com>
2022-08-12 16:24:53 +0300

    Fix issues

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 --script that lists all shows contained in the database hbtn_0d_tvshows.
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT OUTER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT a.title, b.genre_id
+FROM tv_shows AS a
+LEFT JOIN tv_show_genres AS b
+ON a.id = b.show_id
+ORDER BY a.title ASC, b.genre_id ASC;

a508074c030da80173ba170e812ccb7abd7bd36c
Victoria-20 <victoriaocon111@gmail.com>
2022-09-07 10:50:46 +0300

    Arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(0);
+} else {
+
+  let array = process.argv.filter(elem => parseInt(elem));
+  array = array.filter(elem => elem < Math.max(...array));
+  console.log(Math.max(...array));
+}

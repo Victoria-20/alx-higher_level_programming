f63be2d1c71e758d353736ceb64e7e0555c5e953
Victoria-20 <victoriaocon111@gmail.com>
2022-09-07 11:15:40 +0300

    Fix semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,6 @@
 if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
-
   let array = process.argv.filter(elem => parseInt(elem));
   array = array.filter(elem => elem < Math.max(...array));
   console.log(Math.max(...array));

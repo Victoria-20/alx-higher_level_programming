42b4abf02e6aa7af83cb4d4067f26fd6f8ba1cac
Victoria-20 <victoriaocon111@gmail.com>
2022-09-13 12:49:37 +0300

    8. Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 
+exports.esrever = function (list) {
+// Returns the reversed version of a list
+  const reversedList = [];
+  let k = 0;
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList[k] = list[i];
+    k++;
+  }
+  return (reversedList);
+};

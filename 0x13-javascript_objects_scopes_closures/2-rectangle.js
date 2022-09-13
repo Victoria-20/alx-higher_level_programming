3eb7628bf4c972a9f91399e4ed2be568ab71150e
Victoria-20 <victoriaocon111@gmail.com>
2022-09-13 11:47:10 +0300

    2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 
+/* Creates a class Rectangle */
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};

67a10426fac646eaf11e7e4997f9a9ecb14d311d
Victoria-20 <victoriaocon111@gmail.com>
2022-09-13 12:14:27 +0300

    5. Square #0

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
 
+/* Class square inherits from class Rectangle */
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};

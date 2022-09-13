fe396438a5b5a17a52a0e15ffa6c0ee32ce1677d
Victoria-20 <victoriaocon111@gmail.com>
2022-09-13 12:24:07 +0300

    6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,2 +1,20 @@
 #!/usr/bin/node
 
+/* Class Square that defines a square */
+
+const Square5 = require('./5-square');
+
+module.exports = class Square extends Square5 {
+  charPrint (c) {
+    let ch = 'X';
+    if (c) {
+      ch = c;
+    }
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(ch);
+      }
+      console.log('');
+    }
+  }
+};

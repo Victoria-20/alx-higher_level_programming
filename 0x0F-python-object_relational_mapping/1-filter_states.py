b0d4025647bcb5db84b60fa21b3102ed74e3f604
Victoria-20 <victoriaocon111@gmail.com>
2022-09-16 16:20:01 +0300

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python3
+""" lists all states with a name starting with N from the database """
 
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) == 4):
+        user = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        # Connect to database
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=user,
+            passwd=password,
+            db=database,
+            charset="utf8")
+
+        cur = db.cursor()
+        # Execute the SQL query
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            if row[1][0] == "N":
+                print(row)
+
+        # Close all cursors
+        cur.close()
+        # Close db connection
+        db.close()

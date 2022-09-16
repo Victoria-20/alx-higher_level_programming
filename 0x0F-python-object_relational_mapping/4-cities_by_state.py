811a09e456b2a1ea374f656d2fa5464e02d93961
Victoria-20 <victoriaocon111@gmail.com>
2022-09-16 17:05:12 +0300

    4. Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,2 +1,36 @@
 #!/usr/bin/python3
+""" lists all states with a name starting with N from the database """
 
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) == 4):
+        user = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+
+        # Connect to database
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=user,
+            passwd=password,
+            db=database,
+            charset="utf8")
+
+        cur = db.cursor()
+        # Execute the SQL query
+        cur.execute("SELECT id, name, \
+            (SELECT name FROM states WHERE id = state_id)\
+                FROM cities ORDER BY id ASC")
+
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+
+        # Close all cursors
+        cur.close()
+        # Close db connection
+        db.close()

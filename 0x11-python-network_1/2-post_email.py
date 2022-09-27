7656e3429f5c4767b2625014a7d794e0404d8c5e
Victoria-20 <victoriaocon111@gmail.com>
2022-09-27 09:55:31 +0300

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" takes in URL and an email, sends POST request to the URL
+    with email as a parameter, display body of response
+    use packages urllib and sys
+"""
+
+if __name__ == "__main__":
+    import urllib
+    from urllib.request import urlopen
+    import sys
+
+    email = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(email).encode("ascii")
+
+    with urlopen(sys.argv[1], data) as response:
+        print(response.read().decode("utf-8"))

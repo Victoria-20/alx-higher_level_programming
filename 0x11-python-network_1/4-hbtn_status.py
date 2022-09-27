3aae3fb1cae383db97f3316de059f24f6576c88f
Victoria-20 <victoriaocon111@gmail.com>
2022-09-27 10:14:11 +0300

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status """
+
+if __name__ == "__main__":
+    import requests
+
+    res = requests.get("https://alx-intranet.hbtn.io/status")
+    text = res.text
+    print("Body response:")
+    print(f"\t- type: {type(text)}")
+    print(f"\t- content: {text}")

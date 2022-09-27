3105c9445d54ef4731736e4df0b495120cc47f11
Victoria-20 <victoriaocon111@gmail.com>
2022-09-27 10:13:34 +0300

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" takes in URL, sends request to the URL
+    display body of response
+    use packages urllib and sys
+"""
+
+if __name__ == "__main__":
+    from urllib.request import urlopen, error
+    import sys
+
+    try:
+        with urlopen(sys.argv[1], data) as response:
+            print(response.read().decode("utf-8"))
+    except error.HTTPError as err:
+        print("Error code:", err.code)

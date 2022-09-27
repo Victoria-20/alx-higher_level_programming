fc4823aa501863f19d005d0911c5d5e2e77acee3
Victoria-20 <victoriaocon111@gmail.com>
2022-09-27 10:38:22 +0300

    8. Search API

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" takes in URL and an email, sends POST request to the URL
+    with email as a parameter, display body of response
+    use packages urllib and sys
+"""
+
+if __name__ == "__main__":
+    from requests
+    import sys
+
+    email = {"email": sys.argv[2]}
+
+    res = requests.post(sys.argv[1], data=email)
+    print(res.text)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" takes in URL, sends request to the URL
+    display body of response
+    use packages urllib and sys
+"""
+
+if __name__ == "__main__":
+    from requests
+    import sys
+
+    res = requests.get(sys.argv[1])
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)

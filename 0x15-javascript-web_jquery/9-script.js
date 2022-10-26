8ac283ffaa1be040111c808872b3fe91d072d0d7
Victoria-20 <victoriaocon111@gmail.com>
2022-10-26 21:59:37 +0300

    more files

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+$(function () {
+    $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, textStatus) {
+      if (textStatus === 'success') {
+        $('#sf_wind_speed').text(data.query.hello);
+      }
+    });
+  });

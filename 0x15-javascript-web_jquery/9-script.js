8df7b3a5b47685cf4ff673409698c678877600f5
Victoria-20 <victoriaocon111@gmail.com>
2022-10-27 15:57:03 +0300

    add 9

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,13 +1,6 @@
 const $ = window.$;
-$.ajax({
-  type: '',
-  url: 'https://fourtonfish.com/hellosalut/?lang=fr',
-  dataType: "jsonp",
-  success: (response) => {
-    console.log("Not working")
-    $('DIV#hello').text(response.hello);
-  },
-  error: () => {
-    console.log('Error loading orders');
-  }
+$(document).ready(function () {
+  $.get("https://fourtonfish.com/hellosalut/?lang=fr", (data, textStatus) => {
+    $("DIV#hello").text(data.hello);
+  });
 });
